1c66d181716ffe9ccee65c39b7b5a4f082f32512
Hugo Bermudez <hugobermudez@Hugos-Mac-mini.local>
2024-11-04 23:44:21 +0100

    fix: build dist dir

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias:[ 
-      { find : '@', replacement: fileURLToPath(new URL('/src', import.meta.url)) }
+      { find : '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
     ]
   },
+  build: {
+    outDir: 'dist'
+  }
 })
