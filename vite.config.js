59e519f81fd2998bcc1f198b516bab4313d448e5
Hugo Bermudez <hugobermudez@Hugos-Mac-mini.local>
2024-11-04 23:19:59 +0100

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      find : '@', replacement: fileURLToPath(new URL('./src', import.meta.url)),
-    },
+    alias:[ 
+      { find : '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+    ]
   },
 })
