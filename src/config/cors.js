a88d1c387f7a315080e7eb0f9cedf0404fe28f8f
Hugo Bermúdez <hugo.bermudez@me.com>
2024-11-06 21:44:52 +0100

    fix: cors errors adding config

---
+++ src/config/cors.js
@@ -0,0 +1,5 @@
+export const ACCEPTED_ORIGINS = [
+    'https://www.mascotasdana.es',
+    'https://mascotasdana.es',
+    'http://localhost:5173'
+]
--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import petsRoutes from './routes/pets.js';
+import { ACCEPTED_ORIGINS } from './config/cors.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: process.env.ENVIRONMENT === 'development' 
-          ? '*' : process.env.PROD_FRONT_URL
-}
+app.use(cors({
+  origin: (origin, callback) => {
+        
+    if (!origin || ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true);
+    }
+    
+    return callback(null, false);
+  }
+}));
 
-app.use(cors(corsConfig));
 app.use(express.json());
 
 app.use('/api/pets', petsRoutes);
@@ -21,3 +27,4 @@ app.use('/api/pets', petsRoutes);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
