f42f18fa6043ce9d794c5263f71709550686e135
Jose Luis <cabrera@inkuba.com>
2024-11-10 11:21:57 +0100

    fix mismatch error

--- components/CardPet.vue
+++ components/CardPet.vue
@@ -6,54 +6,57 @@ const props = defineProps<{
 
 <template>
   <div>
-    <NuxtLink :to="{ name: 'mascota-id', params: { id: pet.id } }">
-      <div class="card bg-base-100 shadow-xl h-full">
-        <figure>
-          <div
-            class="absolute top-2 right-2 badge badge-lg text-base-100 badge-accent"
-            :class="{
-              'badge-accent': pet.status === 'lost',
-              'badge-info': pet.status === 'found',
-            }"
-          >
-            {{ pet.status === "lost" ? "Perdido" : "Se busca dueño" }}
-          </div>
+    <div class="card bg-base-100 shadow-xl h-full">
+      <figure>
+        <div
+          class="absolute top-2 right-2 badge badge-lg text-base-100 badge-accent"
+          :class="{
+            'badge-accent': pet.status === 'lost',
+            'badge-info': pet.status === 'found',
+          }"
+        >
+          {{ pet.status === "lost" ? "Perdido" : "Se busca dueño" }}
+        </div>
+        <NuxtLink :to="{ name: 'mascota-id', params: { id: pet.id } }">
           <img
             loading="lazy"
             class="w-full h-80 object-cover"
             :src="pet.images[0]"
             :alt="pet.name"
           />
-        </figure>
-        <div class="card-body">
-          <div class="card-title">
-            <div class="flex w-full justify-between">
-              <div class="flex items-center gap-2">
-                <Icon name="bxs:map" />
-                <p>{{ pet.location }}</p>
-              </div>
-              <div class="flex items-center gap-2">
-                <Icon name="mdi-light:calendar" />
-                <p>{{ formatDate(pet.date) }}</p>
-              </div>
+        </NuxtLink>
+      </figure>
+      <div class="card-body">
+        <div class="card-title">
+          <div class="flex w-full justify-between">
+            <div class="flex items-center gap-2">
+              <Icon name="bxs:map" />
+              <p>{{ pet.location }}</p>
+            </div>
+            <div class="flex items-center gap-2">
+              <Icon name="mdi-light:calendar" />
+              <p>{{ formatDate(pet.date) }}</p>
             </div>
           </div>
-          <p class="flex items-center gap-3">
-            <Icon name="fluent-mdl2:contact" />
-            <span>{{ pet.contact }}</span>
-          </p>
-          <p>
-            {{ pet.description }}
-          </p>
+        </div>
+        <p class="flex items-center gap-3">
+          <Icon name="fluent-mdl2:contact" />
+          <span>{{ pet.contact }}</span>
+        </p>
+        <p>
+          {{ pet.description.replace(/\n/g, " ").replace(/\s+/g, " ").trim() }}
+        </p>
 
-          <div class="card-actions justify-end">
-            <a class="btn btn-link text-base no-underline">
-              Ver detalles
-              <Icon name="mdi:arrow-up-thin" class="rotate-90" size="1.5rem" />
-            </a>
-          </div>
+        <div class="card-actions justify-end">
+          <NuxtLink
+            class="btn btn-link text-base no-underline"
+            :to="{ name: 'mascota-id', params: { id: pet.id } }"
+          >
+            Ver detalles
+            <Icon name="mdi:arrow-up-thin" class="rotate-90" size="1.5rem" />
+          </NuxtLink>
         </div>
       </div>
-    </NuxtLink>
+    </div>
   </div>
 </template>
