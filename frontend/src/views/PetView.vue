f4f56c60007165b4d05c1cdf5b1d21c48b7a4ea1
Hugo Bermúdez <hugo.bermudez@me.com>
2024-11-11 22:50:34 +0100

     delete old frontend

--- frontend/src/views/PetView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <AddMenu/>
-    <PetPage/>
-</template>
-<script setup lang="ts">
-    import AddMenu from '../components/AddMenu.vue';
-    import PetPage from '../components/PetPage.vue';
-</script>
-
--- frontend/src/views/RegisterView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-
-<RegisterForm/>
-
-</template>
-
-<script setup>
-import RegisterForm from '@/components/forms/PetRegister.vue'
-</script>
