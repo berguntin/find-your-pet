c21a390b82fd616f74232a41d340253383ea85b5
Hugo Bermúdez <hugo.bermudez@me.com>
2024-11-16 18:38:49 +0100

    add: favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
         head: {
             title: 'Dana Dogs',
             meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }]
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
         }
     },
     tailwindcss: {
