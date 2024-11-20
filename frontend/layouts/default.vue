b9975a8d6ff39fff94d4e9eec9b312cf4b71a66c
Hugo Bermúdez <hugo.bermudez@me.com>
2024-11-20 21:09:56 +0100

    add: speed insights component

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
     const props = defineProps<{
         modelValue?: boolean
     }>()
 </script>
 
 <template>
+    <SpeedInsights/>
     <TheHeader />
     <main class="container mx-auto bg-base-100 min-h-lvh">
         <slot />
